ae65c5e698e3860bf1e3adbff02293865a1714c5
NightM <zzhhgo@gmail.com>
2013-03-31 22:10:28 +0800

    Send weibo

---
+++ weibo4j/examples/testapp/main.java
@@ -0,0 +1,18 @@
+package testapp;
+
+import weibo4j.Timeline;
+import weibo4j.Weibo;
+import weibo4j.http.AccessToken;
+import weibo4j.model.Status;
+import weibo4j.model.WeiboException;
+
+public class main {
+	public static void main(String[] args) throws WeiboException {
+		// TODO Auto-generated method stub
+		Weibo weibo = new Weibo();
+		String access_token = "2.00fXNMxBW2d72D9ad69f14adBQcGnC";// 这里对应accessToken=XXXXXXXXXXXXXXXXXXX的值
+		weibo.setToken(access_token);
+		Timeline tm = new Timeline();
+		Status status = tm.UpdateStatus("哈哈~~");// 自己所发的微博内容，不超过140个字！
+	}
+}
--- weibo4j/examples/weibo4j/examples/timeline/UpdateStatus.java
+++ weibo4j/examples/weibo4j/examples/timeline/UpdateStatus.java
@@ -1,4 +1,4 @@
-package weibo4j.examples.timeline;
+/*package weibo4j.examples.timeline;
 
 import weibo4j.Timeline;
 import weibo4j.examples.oauth2.Log;
@@ -10,6 +10,29 @@ public class UpdateStatus {
 	public static void main(String[] args) {
 		String access_token = args[0];
 		String statuses = args[1];
+		Timeline tm = new Timeline();
+		tm.client.setToken(access_token);
+		try {
+			Status status = tm.UpdateStatus(statuses);
+			Log.logInfo(status.toString());
+		} catch (WeiboException e) {
+			e.printStackTrace();
+		}	}
+
+}*/
+
+package weibo4j.examples.timeline;
+
+import weibo4j.Timeline;
+import weibo4j.examples.oauth2.Log;
+import weibo4j.model.Status;
+import weibo4j.model.WeiboException;
+
+public class UpdateStatus {
+
+	public static void main(String[] args) {
+		String access_token = "2.00fXNMxBW2d72D9ad69f14adBQcGnC";
+		String statuses = "O(∩_∩)O哈哈哈~";
 		Timeline tm = new Timeline();
 		tm.client.setToken(access_token);
 		try {
